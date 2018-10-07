0663c118716251bd570d6b8108f754704581c6c3
Heverton13 <evertinho10.hg@gmail.com>
2018-10-07 16:58:50 -0300

    Projeto com telas atualizadas e primeiras ligações inciadas, está com erro no main.

---
+++ src/controladores/ControladorPrincipal.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controladores;
+
+import com.jfoenix.controls.JFXButton;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author Heverton
+ */
+public class ControladorPrincipal implements Initializable {
+
+    @FXML
+    private BorderPane borderPrincipal;
+    @FXML
+    private Label labelPrincipal;
+    @FXML
+    private JFXButton BtInicio;
+    @FXML
+    private JFXButton Btlogin;
+    @FXML
+    private JFXButton BtSolicitante;
+    @FXML
+    private JFXButton BtAmostra;
+    @FXML
+    private JFXButton BtHistorico;
+    @FXML
+    private JFXButton BtPedido;
+    @FXML
+    private JFXButton BtAjuda;
+    @FXML
+    private JFXButton BtSair;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    @FXML
+    
+    private void cadastroSolcitante(){
+        
+        try {
+            Parent cadastroSolicitante = FXMLLoader.load(getClass().getResource("/telasFX/CadastroSolicitante"));
+            borderPrincipal.setCenter(cadastroSolicitante);
+            labelPrincipal.setText("Cadastro de Solitantes");
+        } catch (IOException ex) {
+            Logger.getLogger(ControladorPrincipal.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+    @FXML
+    private void adicionarAmostra(){
+        
+        try {
+            Parent adicionarAmostra = FXMLLoader.load(getClass().getResource("/telasFX/TelaAmostra_1.fxml"));
+            borderPrincipal.setCenter(adicionarAmostra);
+            labelPrincipal.setText("Cadastro de Amostras");
+        } catch (IOException ex) {
+            Logger.getLogger(ControladorPrincipal.class.getName()).log(Level.SEVERE, null, ex);
+        }
+     
+    }
+}
---
+++ src/main/Principal.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Heverton
+ */
+public class Principal extends Application {
+    
+    public static void main(String[] args){
+        launch(args);
+    }
+    
+    public void start(Stage stage) throws Exception {
+        
+		Parent root = FXMLLoader.load(getClass().getResource("TelaPrincipal.fxml"));
+                Scene scene = new Scene(root);
+                
+                stage.setScene(scene);
+                stage.setTitle("Gerenciar Amostras");
+                stage.show();
+    }
+    
+}
