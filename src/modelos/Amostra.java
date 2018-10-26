d1f851da6bfea178ca47e59b905513bc3e449db2
Aluno <Aluno@10.77.22.174>
2018-10-26 14:15:54 -0200

    ALGUMAS ATUALIZAÇÕES NO SQL E DAO AMOSTRA E CLASSE AMOSTRA

--- src/modelos/Amostra.java
+++ src/modelos/Amostra.java
@@ -10,6 +10,8 @@ import java.util.Date;
 public class Amostra {
     
     private String id_amostra;
+    private int id_solicitante;
+    private int id_responsavel;
     private String descricao;
     private int frascos;
     private String observacoes;
@@ -25,15 +27,17 @@ public class Amostra {
     };
     private String analises_aequeridas;
 
-    public Amostra(String id_amostra, String descricao, int frascos, String observacoes, Date data_entrada, String id_Solicitante, String id_Responsavel, String ar) {
+    public Amostra(String id_amostra, int id_solicitante, int id_responsavel, String descricao, int frascos, String observacoes, Date data_entrada, String id_Solicitante, String id_Responsavel, String analises_aequeridas) {
         this.id_amostra = id_amostra;
+        this.id_solicitante = id_solicitante;
+        this.id_responsavel = id_responsavel;
         this.descricao = descricao;
         this.frascos = frascos;
         this.observacoes = observacoes;
         this.data_entrada = data_entrada;
         this.id_Solicitante = id_Solicitante;
         this.id_Responsavel = id_Responsavel;
-        this.analises_aequeridas = ar;
+        this.analises_aequeridas = analises_aequeridas;
     }
 
     public String getId_amostra() {
@@ -42,6 +46,22 @@ public class Amostra {
 
     public void setId_amostra(String id_amostra) {
         this.id_amostra = id_amostra;
+    }
+
+    public int getId_solicitante() {
+        return id_solicitante;
+    }
+
+    public void setId_solicitante(int id_solicitante) {
+        this.id_solicitante = id_solicitante;
+    }
+
+    public int getId_responsavel() {
+        return id_responsavel;
+    }
+
+    public void setId_responsavel(int id_responsavel) {
+        this.id_responsavel = id_responsavel;
     }
 
     public String getDescricao() {
@@ -99,6 +119,7 @@ public class Amostra {
     public void setAnalises_aequeridas(String analises_aequeridas) {
         this.analises_aequeridas = analises_aequeridas;
     }
+
     
         
 }
--- src/persistencia/AmostraDAO.java
+++ src/persistencia/AmostraDAO.java
@@ -24,7 +24,7 @@ public class AmostraDAO {
     
     private Conexao con = new Conexao();
     
-    private final String INSERTAMOSTRA = "INSERT INTO AMOSTRA (IDENTIFICACAO_AMOSTRA, DESCRICAO, FRACOS,OBSERVACOES,DATA_ENTRADA,TIPO_AMOSTRA) VALUES (?,?,?,?,?,?)";
+    private final String INSERTAMOSTRA = "INSERT INTO AMOSTRA (IDENTIFICACAO_AMOSTRA, ID_SOLICITANTE, ID_REPONSAVEL, DESCRICAO, FRACOS,OBSERVACOES,DATA_ENTRADA,TIPO_AMOSTRA) VALUES (?,?,?,?,?,?)";
     private final String UPDATEAMOSTRA = "UPDATE AMOSTRA SET IDENTIFICACAO_AMOSTRA = ? WHERE IDENTIFICACAO_AMOSTRA = ?";
     private final String DELETEAAMOSTRA = "DELETE FROM AMOSTRA WHERE IDENTIFICACAO_AMOSTRA = ?";
     private final String LISTAMOSTRA = "SELECT * FROM AMOSTRA ORDER BY IDENTIFICACAO_AMOSTRA";
@@ -47,13 +47,15 @@ public class AmostraDAO {
             Responsavel r = null;
             
             preparaInstrucao.setString(1, a.getId_amostra().toUpperCase());
-            preparaInstrucao.setString(2, a.getDescricao().toUpperCase());
-            preparaInstrucao.setInt(3, a.getFrascos());
-            preparaInstrucao.setString(4, a.getObservacoes().toUpperCase());
-            preparaInstrucao.setDate(5, (Date) a.getData_entrada());
-            preparaInstrucao.setString(6, a.getId_Solicitante().toUpperCase());
-            preparaInstrucao.setString(7, a.getId_Responsavel().toUpperCase());
-            preparaInstrucao.setString(8, a.getAnalises_aequeridas());
+            preparaInstrucao.setInt(2, a.getId_solicitante());
+            preparaInstrucao.setInt(3, a.getId_responsavel());
+            preparaInstrucao.setString(4, a.getDescricao().toUpperCase());
+            preparaInstrucao.setInt(5, a.getFrascos());
+            preparaInstrucao.setString(6, a.getObservacoes().toUpperCase());
+            preparaInstrucao.setDate(7, (Date) a.getData_entrada());
+            preparaInstrucao.setString(8, a.getId_Solicitante().toUpperCase());
+            preparaInstrucao.setString(9, a.getId_Responsavel().toUpperCase());
+            preparaInstrucao.setString(10, a.getAnalises_aequeridas());
 
             preparaInstrucao.execute();
             con.desconecta();
@@ -125,7 +127,7 @@ public class AmostraDAO {
 			
             while (rs.next()) { 
                 
-                Amostra a = new Amostra(rs.getString("IDENTIFICACAO_AMOSTRA"), rs.getString("DESCRICAO"), rs.getInt("FRASCOS"), rs.getString("OBSERVACOES"), rs.getDate("DATA_ENTRADA"), rs.getString("SOLICITANTE_ACADEMICO"), rs.getString(LISTAMOSTRA),rs.getString("TIPO_AMOSTRA"));
+                Amostra a = new Amostra(rs.getString("IDENTIFICACAO_AMOSTRA"),rs.getInt("ID_Amostra"),rs.getInt("ID_RESPONSAVEL"), rs.getString("DESCRICAO"), rs.getInt("FRASCOS"), rs.getString("OBSERVACOES"), rs.getDate("DATA_ENTRADA"), rs.getString("SOLICITANTE_ACADEMICO"), rs.getString(LISTAMOSTRA),rs.getString("TIPO_AMOSTRA"));
 		lista.add(a); 
                 
             }
